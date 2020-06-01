3cb9a23fefe608f445d91fb7491b803603b77504
Sebastian <avaven@gmail.com>
2020-06-01 19:32:50 +0200

    Add api part for starships

--- src/api/starships.ts
+++ src/api/starships.ts
@@ -1,9 +1,32 @@
 import axios from 'axios';
 
-const getAllStarships = async (): Promise<Starship[]> => {
-  const { data } = await axios.get('https://swapi.dev/api/starships');
-  console.log(data);
+const BASE_URL = 'https://swapi.dev/api';
+const URL = `${BASE_URL}/starships`;
+
+interface StarshipsResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Starship[];
+}
+
+type StarshipResponse = Starship;
+
+const getAllStarships = async (page = 1): Promise<StarshipsResponse> => {
+  const { data } = await axios.get<StarshipsResponse>(`${URL}/?page=${page}`);
   return data;
 };
 
-export { getAllStarships };
+//Default id is 2 because no 1 is empty result
+const getSingleStarship = async (id = 2): Promise<StarshipResponse> => {
+  const { data } = await axios.get(`${URL}/${id}`);
+  return data;
+};
+
+const searchForStarship = async (searchPhrase: string): Promise<StarshipsResponse> => {
+  const { data } = await axios.get<StarshipsResponse>(`${URL}/?search=${searchPhrase}`);
+
+  return data;
+};
+
+export { getAllStarships, getSingleStarship, searchForStarship };
