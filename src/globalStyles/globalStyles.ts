516200ed5edd36eeb07433edf05f53b6083fa00d
Sebastian <avaven@gmail.com>
2020-06-01 14:46:05 +0200

    Swap fonts for futuristic looking one

--- src/globalStyles/globalStyles.ts
+++ src/globalStyles/globalStyles.ts
@@ -2,31 +2,23 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   
+  *, *:before, *:after {  
+    box-sizing: inherit;
+  } 
+  
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     display: flex;
-    font-family: 'Roboto', sans-serif;
-    
-    h1,h2,h3 {
-      font-family: 'Montserrat', sans-serif;
-    }
-    
+    font-family: 'Electrolize', sans-serif;
   }
-  
-  *, *:before, *:after {
-    box-sizing: inherit;
-  } 
   
   #root {
     padding: 0;
     width: 100%;
     }
-    
-    .fullscreen {
-      z-index: 4;
-    }
+
 `;
 
 export default GlobalStyle;
